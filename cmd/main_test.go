1ceed4a27d26003bc69a254c3a5ba17400d0e9c0
Jonas Felix <jonas.felix@felixideas.ch>
2023-10-04 17:21:17 +0200

    -v option test

--- cmd/main_test.go
+++ cmd/main_test.go
@@ -12,15 +12,16 @@ func TestRetryCommand(t *testing.T) {
 		t.Errorf("Expected error, got none")
 	}
 
+	args = []string{"retry", "-c", "3", "-v", "-i", "1s", "-t", "1s", "--", "echo error 1>&2; false"}
+	err = app.Run(args)
+	if err == nil {
+		t.Errorf("Expected error, got none")
+	}
+
 	args = []string{"retry", "-c", "1", "-i", "1s", "-t", "1s", "--", "echo success; true"}
 	err = app.Run(args)
 	if err != nil {
 		t.Errorf("Expected no error, got %v", err)
 	}
 	
-	args = []string{"retry", "-c", "3", "-v", "-i", "1s", "-t", "1s", "--", "echo error 1>&2; false"}
-	err = app.Run(args)
-	if err == nil {
-		t.Errorf("Expected error, got none")
-	}
 }
